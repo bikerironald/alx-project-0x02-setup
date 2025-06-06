6062a97aad2c193d6efab434ab753394408367b3
bikerironald <bikerironald@gmail.com>
2025-06-06 21:57:09 +0300

    Create a Reusable Card Component

--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from '../components/layout/Header';
+import Card from '../components/common/Card';
 
 const Home: React.FC = () => {
   return (
@@ -7,9 +8,17 @@ const Home: React.FC = () => {
       <Header />
       <main className="flex flex-col items-center justify-center min-h-screen bg-gray-50">
         <h1 className="text-3xl font-bold mb-4">Welcome to the ALX Project 2!</h1>
-        <p className="text-lg text-gray-700">
+        <p className="text-lg text-gray-700 mb-8">
           This is the home page of your Next.js application.
         </p>
+        <Card
+          title="Reusable Card 1"
+          content="This is the content for the first card component."
+        />
+        <Card
+          title="Reusable Card 2"
+          content="This is the content for the second card component."
+        />
       </main>
     </>
   );
